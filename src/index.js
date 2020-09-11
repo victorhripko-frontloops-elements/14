927a5220acd78453fb6b2bd3c715a8713668423c
Victor Hripko <hripko.v@soft2bet.com>
2020-09-11 18:06:09 +0300

    pre done

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import './style.scss';
 (() => {
 
   const form = document.querySelector('.form');
-  const inputs = document.querySelectorAll('.form__input');
+  const inputs = form.querySelectorAll('.form__input');
+  const password = form.querySelector('[name="password"]');
+  const confirmPassword = form.querySelector('[name="confirm_password"]');
+
 
   form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const passwordVal = password.value;
+    const confirmPasswordVal = confirmPassword.value;
+
     inputs.forEach((input) => {
       input.classList.toggle('is-invalid', !input.validity.valid);
     });
 
-    evt.preventDefault();
+    confirmPassword.classList.toggle('is-invalid', passwordVal && confirmPasswordVal !== passwordVal);
   }, false);
 
 })();
